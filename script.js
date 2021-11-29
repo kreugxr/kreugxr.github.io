83218abeb61d96cf7bd6b18546323bbea59ef125
CEO WHATSAPP <kreugxr@icloud.com>
2021-11-29 01:46:14 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function Switch() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+
+
+    var linkColor = document.a;
+    linkColor.classList.toggle("link-color")
+ }
