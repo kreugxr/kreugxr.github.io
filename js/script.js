a13090c21cf7d2fc7c6e44edcd58210bebaa3775
CEO WHATSAPP <77470204+kreugxr@users.noreply.github.com>
2021-12-16 05:11:10 -0300

    ~

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Switch() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+
+    var linkColor = document.a;
+    linkColor.classList.toggle("link-color")
+ }
