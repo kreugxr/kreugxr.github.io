170277a8ad1f3ca9acb74585ad53a059a300495b
CEO WHATSAPP <77470204+kreugxr@users.noreply.github.com>
2021-12-16 04:56:24 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function Switch() {
-    var element = document.body;
-    element.classList.toggle("light-mode");
-
-
-    var linkColor = document.a;
-    linkColor.classList.toggle("link-color")
- }
